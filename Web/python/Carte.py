7abe3b65caec3d0f1386a092d77db67f34623760
22totoche22 <adrien.caillet2@gmail.com>
2018-05-03 10:25:05 +0200

    [carte] modificates the carte python script

--- Web/python/Carte.py
+++ Web/python/Carte.py
@@ -13,57 +13,121 @@ def index():
 
 def carte():
     vcarte = '''
-        <section id="carte">
-            <link rel="stylesheet" href="https://unpkg.com/leaflet@1.3.1/dist/leaflet.css"
-   integrity="sha512-Rksm5RenBEKSKFjgI3a41vrjkw4EVPlJ3+OiI65vTjIdo9brlAacEuKOiQ5OFh7cOI1bkDwLqdLw3Zg0cRJAAQ=="
-   crossorigin=""/>
-     <script src="https://unpkg.com/leaflet@1.3.1/dist/leaflet.js"
-   integrity="sha512-/Nsx9X4HebavoBvEBuyp3I7od5tA0UzAxs+j83KgC8PU0kgB4XiK4Lfe4y4cgBtaRJQEIFCW+oC506aPT2L1zw=="
-   crossorigin=""></script>
-
-</head>
-<body>
-    <div id="mapid" style="height : 800px;width : 800px;position : relative"></div>
-    <script>
-    var mymap = L.map('mapid').setView([51.505, -0.09], 13);
-    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox.streets',
-    accessToken: 'pk.eyJ1IjoiMjJ0b3RvY2hlMjIiLCJhIjoiY2pmeHppZ3RyMXA0czJ5cWtiYWI5a3JzdCJ9.AWDYryAXZ9DWviaxWy_nuw'
-}).addTo(mymap);
-var marker = L.marker([51.5, -0.09]).addTo(mymap);
-var circle = L.circle([51.508, -0.11], {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.5,
-    radius: 500
-}).addTo(mymap);
-var polygon = L.polygon([
-    [51.509, -0.08],
-    [51.503, -0.06],
-    [51.51, -0.047]
-]).addTo(mymap);
-
-marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
-circle.bindPopup("I am a circle.");
-polygon.bindPopup("I am a polygon.");
-
-
-var popup = L.popup();
-
-function onMapClick(e) {
-    popup
-        .setLatLng(e.latlng)
-        .setContent("You clicked the map at " + e.latlng.toString())
-        .openOn(mymap);
+         <style>
+      #map {
+        height: 100%;
+      }
+      /* Optional: Makes the sample page fill the window. */
+      html, body {
+        height: 100%;
+        margin: 0;
+        padding: 0;
+      }
+       #floating-panel {
+       position: absolute;
+        bottom: 20px;
+        left: 0.5%;
+        z-index: 5;
+        background-color: #fff;
+        padding: 5px;
+        border: 1px solid #999;
+        text-align: center;
+        font-family: 'Roboto','sans-serif';
+        line-height: 30px;
+        padding-left: 10px;
 }
+        label {
+        display : inline-block}
+        #1{
+        display:inline}
+    </style>
+  </head>
+  <body>
+      
+     <form id="floating-panel" method="POST" action = "fcontacter" enctype="multipart/form-data">
+      <div id=1><label class="" >Latitude : </label> <input id=latitude type="text" name="latitude" value="" disabled></div>
+        <div id=2><label class="" >Longitude : </label> <input id=longitude type="text" name="longitude" value="" disabled></div>
+        <div id=2><label class="" >Adresse : </label> <input id=adresse type="text" name="adresse" value=""disabled ></div>
+        <div id=3><input  type="submit"  value="valider"></div>
+      </form>
+      <div id="map"></div>
+    <script>
+      function initMap() {
+            var geocoder = new google.maps.Geocoder();
+            var myLatlng = {lat: 43.6, lng: 1.433333};
+            var map = new google.maps.Map(document.getElementById('map'), {
+                            zoom: 10,
+                            center: myLatlng
+            });
 
-mymap.on('click', onMapClick);
-</script>
-</body>
+            map.addListener('click', function(event) {
+                            placeMarker(event.latLng, map,geocoder);
+            });
+            latitude.value = "";
+            longitude.value= "";
+            adresse.value="";
+      }
+      var marker;
+      var latitude = document.getElementById("latitude");
+      var longitude = document.getElementById("longitude");
+      var adresse = document.getElementById("adresse");
+      function placeMarker(location, map,geocoder) {
+                    adresse.value = "";
+                    if(marker){
+                        marker.setPosition(location);
+                    }
+                    else{
+                        marker = new google.maps.Marker({
+                        position: location,
+                        map: map
+                        });
+                    }
+            map.panTo(marker.getPosition());
+            latitude.value = location.lat();
+            longitude.value = location.lng();
+            geocoder.geocode( { 'location': {lat : parseFloat(location.lat()),lng : parseFloat(location.lng())}}, function(results, status) {
 
-        </section>
+                    if (status === 'OK') {
+                        if (results[0]) {
+
+                                  if (results[1].address_components[0].types[1] == "political"){
+                                          adresse.value = results[1].address_components[0].long_name;
+
+                                    }
+
+
+
+                        }
+                        else {
+                            window.alert('No results found');
+                        }
+                    }
+                    else {
+                            window.alert('Geocoder failed due to: ' + status);
+                    }
+
+            });
+
+
+    }
+
+    </script>
+    <script src="https://developers.google.com/maps/documentation/javascript/examples/marker/marker.js">
+    </script>
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/
+jquery.min.js"></script>
+
+    <script src="https://maps.googleapis.com/maps/api/js?key= AIzaSyAllVFdOZbLMkxQNHMcyCWM3b2TYzASBzQ&callback=initMap"
+    async defer></script>
+  </body>
         '''
 
     return vcarte
+
+
+def fcontacter(latitude='', longitude='', adresse=''):
+    result = "les données envoyées sont : <br />lat :" + latitude
+    result += "<br /> long :" + longitude
+    result += "<br /> adresse :" + adresse
+    return result
+
